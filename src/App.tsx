2efa31933605ffb6f823216f1d62388bf8103b11
ioranadigital <honatuya@gmail.com>
2026-03-07 22:29:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Privacidad from './components/Privacidad'
 
 const queryClient = new QueryClient();
 
@@ -19,7 +18,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-          <Route path="/privacidad" element={<Privacidad />} />
+         
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
