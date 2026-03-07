b8128703d3d8b3ad900686e3552a2ae91cbd56db
ioranadigital <honatuya@gmail.com>
2026-03-07 23:11:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/privacidad" element={<Privacidad />} />
+          <Route path="/terminos" element={<Terminos />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
          
