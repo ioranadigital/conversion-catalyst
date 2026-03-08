edee1feca486add1474279e3f93a2b71ff8904e3
ioranadigital <honatuya@gmail.com>
2026-03-08 01:30:52 +0100

    Update USPSection.tsx

--- src/components/USPSection.tsx
+++ src/components/USPSection.tsx
@@ -42,10 +42,11 @@ const USPSection = () => (
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ delay: i * 0.15 }}
-            className="glass-card p-8 hover:neon-border transition-all duration-500"
+            className="glass-card p-8 hover:neon-border transition-all duration-500"S
           >
-            <div className="w-12 h-12 rounded-lg bg-primary/10 flex items-center justify-center mb-5">
-              <p.icon className="h-6 w-6 text-primary" />
+            {/* color Logo */}
+           <div className="w-12 h-12 rounded-lg bg-[#ebf2f7]/10 border border-[#ebf2f7]/20 flex items-center justify-center mb-5 shadow-[0_0_15px_rgba(235,242,247,0.1)]">
+          <p.icon className="h-6 w-6 text-[#ebf2f7]" /> {/* Dcolor icocno */}
             </div>
             {/* Dentro del map de pillars en USPSection.tsx */}
            <h3 className="text-xl font-bold mb-3 text-[#ebf2f7]">
